354d21cd28b93657ac5ad21fc7e5b6bcf9f115c5
agusTrih <ant3dong@gmail.com>
2020-07-26 17:33:02 +0700

    set up in app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { cretaeStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { reducers } from "./redux/reducers";
+import thunk from "redux-thunk";
+
+import "./App.css";
+
+const store = cretaeStore(reducers, applyMiddleware(thunk));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <Provider store={store}></Provider>;
 }
 
 export default App;
