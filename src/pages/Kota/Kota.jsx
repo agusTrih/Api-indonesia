8d5c15ff7b1769f745685a20f3897d3c4d1f9df1
agusTrih <ant3dong@gmail.com>
2020-07-26 19:49:45 +0700

    add kota

---
+++ src/pages/Kota/Kota.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Kota() {
+    return <div></div>;
+}
+
+export default Kota;
---
+++ src/pages/index.js
@@ -0,0 +1,2 @@
+export * from "./Komunitas/Komunitas";
+export * from "./Kota/Kota";
